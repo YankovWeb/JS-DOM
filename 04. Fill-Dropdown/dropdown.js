1d92e7e4fa6a43773ee88a3f950adfd836d46278
Grigor Yankov <109748669+YankovWeb@users.noreply.github.com>
2022-09-30 22:46:39 +0300

    Update dropdown.js

--- 04. Fill-Dropdown/dropdown.js
+++ 04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+  const item = document.getElementById("newItemText");
+  const itemValue = document.getElementById("newItemValue");
+  const menu = document.getElementById("menu");
+  const select = document.createElement("option");
+  select.textContent = item.value;
+  select.value = itemValue.value;
+  menu.appendChild(select);
+  item.value = "";
+  itemValue.value = "";
 }
