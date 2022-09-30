a28aeba8250fb8842ea19e75ca8a951b15dacec7
Grigor Yankov <109748669+YankovWeb@users.noreply.github.com>
2022-09-30 22:47:18 +0300

    Update app.js

--- 05. Encode-and-Decode-Messages/app.js
+++ 05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const buttons = [...document.getElementsByTagName("button")];
+
+  const firstButton = buttons[0];
+  firstButton.addEventListener("click", encodeAndSend);
+
+  const secoundButton = buttons[1];
+  secoundButton.addEventListener("click", decodeAndRead);
+
+  function encodeAndSend() {
+    let text = document.getElementsByTagName("textarea")[0].value;
+
+    let arrOfChar = text.split("");
+    arrOfChar.forEach((char, i) => {
+      arrOfChar[i] = String.fromCharCode(char.charCodeAt(0) + 1);
+    });
+
+    arrOfChar = arrOfChar.join("");
+    document.getElementsByTagName("textarea")[1].value = arrOfChar;
+
+    document.getElementsByTagName("textarea")[0].value = "";
+
+    console.log("click");
+  }
+
+  function decodeAndRead() {
+    let textForDecoding = document.getElementsByTagName("textarea")[1].value;
+
+    let arrForDecoding = textForDecoding.split("");
+
+    arrForDecoding.forEach((char, i) => {
+      arrForDecoding[i] = String.fromCharCode(char.charCodeAt(0) - 1);
+    });
+    arrForDecoding = arrForDecoding.join("");
+    document.getElementsByTagName("textarea")[1].value = arrForDecoding;
+
+    console.log("click2");
+  }
 }
